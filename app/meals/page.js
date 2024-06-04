3de3b9b5dd9050ef051ffd5185a480ac9187b7e5
herno <herno.russo@gmail.com>
2024-06-04 17:14:06 -0300

    set up meal page header

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,18 +1,23 @@
 import Link from "next/link";
 
+import styles from "./page.module.css";
+
 const MealsPage = () => (
-  <main>
-    <h1>Welcome to the Melas Page!</h1>
-    <Link href="/meals/meal-1">
-      <p>meal 1</p>
-    </Link>
-    <Link href="/meals/meal-2">
-      <p>meal 2</p>
-    </Link>
-    <Link href="/meals/meal-3">
-      <p>meal 2</p>
-    </Link>
-  </main>
+  <>
+    <header className={styles.header}>
+      <h1>
+        Delicious Meals, created{" "}
+        <span className={styles.highlight}>by you</span>
+      </h1>
+      <p>
+        Choose your favorite recipe and cook it yourself. It is easy and fun!
+      </p>
+      <div className={styles.cta}>
+        <Link href="/meals/share">Share Your Favorite Recipe</Link>
+      </div>
+    </header>
+    <main></main>
+  </>
 );
 
 export default MealsPage;
