80825e08d34c598e4b55f91ea814932656af70e8
herno <herno.russo@gmail.com>
2024-06-05 21:35:02 -0300

    Query data from better-sqlite3

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,26 +1,32 @@
 import Link from "next/link";
 
+import { getMeals } from "@/lib/meals";
+
 import { MealGrid } from "./(meal-grid)";
 import styles from "./page.module.css";
 
-const MealsPage = () => (
-  <>
-    <header className={styles.header}>
-      <h1>
-        Delicious Meals, created{" "}
-        <span className={styles.highlight}>by you</span>
-      </h1>
-      <p>
-        Choose your favorite recipe and cook it yourself. It is easy and fun!
-      </p>
-      <p className={styles.cta}>
-        <Link href="/meals/share">Share Your Favorite Recipe</Link>
-      </p>
-    </header>
-    <main>
-      <MealGrid meals={[]} />
-    </main>
-  </>
-);
+const MealsPage = async () => {
+  const meals = await getMeals();
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious Meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main>
+        <MealGrid meals={meals} />
+      </main>
+    </>
+  );
+};
 
 export default MealsPage;
