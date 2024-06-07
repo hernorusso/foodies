05b6290841ae96169544ddcbec96c57b4480c7e4
herno <herno.russo@gmail.com>
2024-06-07 18:06:18 -0300

    get and present meals details

--- app/meals/[meal-slug]/page.js
+++ app/meals/[meal-slug]/page.js
@@ -1,13 +1,35 @@
 import Link from "next/link";
+import Image from "next/image";
 
-const MealDetails = ({ params: { ["meal-slug"]: mealSlug } }) => {
+import { getMealDetail } from "@/lib/meals";
+
+import styles from "./page.module.css";
+
+const MealDetails = ({ params }) => {
+  const { ["meal-slug"]: mealSlug } = params;
+  const { title, image, summary, instructions, creator, creator_email } =
+    getMealDetail(mealSlug);
+
+  const htmlIntructions = instructions.replace(/\n/g, "<br />");
+  const markup = { __html: htmlIntructions };
   return (
-    <main>
-      <h1>Your meal details</h1>
-      <Link href="/meals/share">
-        <p>Share {mealSlug} your meal</p>
-      </Link>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={image} fill alt={"title"} />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{title}</h1>
+          <p className={styles.creator}>
+            by <Link href={`mailto:${creator_email}`}>{creator}</Link>
+          </p>
+          <p className={styles.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p dangerouslySetInnerHTML={markup} className={styles.instructions}></p>
+      </main>
+    </>
   );
 };
 
