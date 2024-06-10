637cbab9bd0f81d7261f20267328ebd76257c6ac
herno <herno.russo@gmail.com>
2024-06-10 11:54:51 -0300

    Create and make image picker interactive

--- app/meals/share/image-picker/image-picker.js
+++ app/meals/share/image-picker/image-picker.js
@@ -1,7 +1,32 @@
+"use client";
+import { useRef } from "react";
 import styles from "./image-picker.module.css";
 
-const ImagePicker = () => {
-  return <p>Image Picker here!</p>;
+const ImagePicker = ({ label, name }) => {
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <div className={styles.controls}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.picker}>
+        <input
+          id={name}
+          type="file"
+          accept="image/jpeg, image/png"
+          className={styles.input}
+          name={name}
+          ref={inputRef}
+        />
+        <button type="button" className={styles.button} onClick={handleClick}>
+          Pick and Image
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export { ImagePicker };
