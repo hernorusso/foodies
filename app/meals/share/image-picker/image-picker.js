ad566d08e9d6b460b145d2fb321854cc5e9f7a2d
herno <herno.russo@gmail.com>
2024-06-10 17:50:15 -0300

    Add a preview file for the selected image

--- app/meals/share/image-picker/image-picker.js
+++ app/meals/share/image-picker/image-picker.js
@@ -1,9 +1,22 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
+
 import styles from "./image-picker.module.css";
 
 const ImagePicker = ({ label, name }) => {
+  const [pickedImage, setPickedImage] = useState(null);
   const imageInput = useRef(null);
+
+  const handleChange = (event) => {
+    const imageFile = event.target.files[0];
+    if (!imageFile) {
+      return setPickedImage(null);
+    }
+    const reader = new FileReader();
+    reader.onload = () => setPickedImage(reader.result);
+    reader.readAsDataURL(imageFile);
+  };
 
   const handleClick = () => {
     imageInput.current.click();
@@ -13,6 +26,12 @@ const ImagePicker = ({ label, name }) => {
     <div className={styles.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={styles.controls}>
+        <div className={styles.preview}>
+          {!pickedImage && <p>No image selected!</p>}
+          {pickedImage && (
+            <Image src={pickedImage} alt="File selected by the user" fill />
+          )}
+        </div>
         <input
           id={name}
           type="file"
@@ -20,6 +39,8 @@ const ImagePicker = ({ label, name }) => {
           className={styles.input}
           name={name}
           ref={imageInput}
+          onChange={handleChange}
+          required
         />
         <button type="button" className={styles.button} onClick={handleClick}>
           Pick and Image
